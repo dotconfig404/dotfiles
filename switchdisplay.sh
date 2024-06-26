124d4f640341e0a7d8bee5a7550c75e41f5dc5f4
dotconfig404 <dotconfig@krutt.org>
2024-06-26 07:21:29 +0200

    external display added

--- switchdisplay.sh
+++ switchdisplay.sh
@@ -2,7 +2,9 @@
 if  xrandr | grep -q "eDP-1 connected primary" 
 then
     xrandr --output DP-2 --mode 3840x1600 --rate 60 --primary --output eDP-1 --off
+    xrandr --output DP-1 --mode 3840x1600 --rate 60 --primary --output eDP-1 --off
 else
+    xrandr --output eDP-1 --mode 1920x1200 --rate 60 --primary --output DP-1 --off
     xrandr --output eDP-1 --mode 1920x1200 --rate 60 --primary --output DP-2 --off
 fi
 
