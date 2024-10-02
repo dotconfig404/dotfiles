039f9746ef73b1a51b2d3d16501fef98a9d0b7e0
dotconfig404 <dotconfig@krutt.org>
2024-10-02 13:43:22 +0200

    added substring history search

---
+++ zsh/.zprofile.d/zsh-history-substring-search.plugin.zsh
@@ -0,0 +1,3 @@
+#0=${(%):-%N}
+#source ${0:A:h}/zsh-history-substring-search/zsh-history-substring-search.zsh
+source $HOME/dotfiles/zsh/.zprofile.d/zsh-history-substring-search/zsh-history-substring-search.zsh
