8cbcb20487f8c465bb67550087ba2833d933b67e
dotconfig404 <dotconfig@krutt.org>
2025-06-25 11:49:10 +0200

    tabs=2spaces

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -7,3 +7,7 @@ vim.api.nvim_set_option("clipboard", "unnamed")
 -- theme
 --vim.cmd 'colorscheme neomodern'
 vim.wo.number = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.bo.softtabstop = 2
