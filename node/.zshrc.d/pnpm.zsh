bdeeac251a4bd8a0f6ca66bdb02e2b8790185fa3
dotconfig404 <dotconfig@krutt.org>
2026-03-18 15:44:22 +0100

    node

---
+++ node/.zshrc.d/pnpm.zsh
@@ -0,0 +1,13 @@
+# PNPM
+# curl -fsSL https://get.pnpm.io/install.sh | sh -
+export PNPM_HOME="/home/$USER/.local/share/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+
+# NVM
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
